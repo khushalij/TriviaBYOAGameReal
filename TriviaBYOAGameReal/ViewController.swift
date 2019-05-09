15ee3b20bf4577a4c03d0808c653fd8bdae0eba7
Khushali Joshi 2021 <kjoshi21@3012-l29555-s.local>
2019-05-09 09:57:10 -0500

    Initial Commit

---
+++ TriviaBYOAGameReal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TriviaBYOAGameReal
+//
+//  Created by Khushali Joshi 2021 on 5/9/19.
+//  Copyright © 2019 Khushali Joshi 2021. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
