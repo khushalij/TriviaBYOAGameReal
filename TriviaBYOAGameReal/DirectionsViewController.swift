302a72002ab10e1a239bc0b4cc1e2c2fb45fe818
Isabella Baum 2019 <ibaum9@3012-l25929-s.local>
2019-05-10 09:40:44 -0500

    directions view controller and continue button

---
+++ TriviaBYOAGameReal/DirectionsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DirectionsViewController.swift
+//  TriviaBYOAGameReal
+//
+//  Created by Isabella Baum 2019 on 5/10/19.
+//  Copyright © 2019 Khushali Joshi 2021. All rights reserved.
+//
+
+import UIKit
+
+class DirectionsViewController: UIViewController {
+
+    @IBAction func continueButtonTapped(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
