d1ebcac0db0be1ffec6e08395ec9991cd1caaa41
Felicia 2019 <fwang9@3012-l29552-s.local>
2019-05-13 10:06:05 -0500

    Added segues to all buttons

---
+++ TriviaBYOAGameReal/QuestionsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  QuestionsViewController.swift
+//  TriviaBYOAGameReal
+//
+//  Created by Felicia Wang 2019 on 5/13/19.
+//  Copyright © 2019 Khushali Joshi 2021. All rights reserved.
+//
+
+import UIKit
+
+class QuestionsViewController: UIViewController {
+
+    @IBOutlet weak var topicLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TriviaBYOAGameReal/TopicViewController.swift
+++ TriviaBYOAGameReal/TopicViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class topicVC: UIViewController {
+class TopicViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
