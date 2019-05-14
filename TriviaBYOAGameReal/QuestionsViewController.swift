733c6f9e2ddb0a16d1a4cfd3996545095227b6e0
Khushali Joshi 2021 <kjoshi21@3012-l29555-s.local>
2019-05-14 10:09:31 -0500

    for loop for animal questions...but still need random generator

--- TriviaBYOAGameReal/QuestionsViewController.swift
+++ TriviaBYOAGameReal/QuestionsViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class QuestionsViewController: UIViewController {
 //arrays for questions
-    let animalsArrayQ: [String] = ["What animal has the longest lifespan?","A newborn kangaroo is about the size of a...?","How many times can a hummingbird flap its wings per second?","What animal has the highest blood pressure?","How mnay noses do slugs have?"]
+    let animalsArrayQ: [String] = ["What animal has the longest lifespan?","A newborn kangaroo is about the size of a...?","How many times can a hummingbird flap its wings per second?","What animal has the highest blood pressure?","How many noses do slugs have?"]
     let historyArrayQ: [String] = ["When was America discovered?","What Europen language was first spoken in the United States?","What is the real name of Uncle Sam?","Which year was CNN established?","In which state did Thomas Edison die?"]
     let famousPeopleArrayQ: [String] = ["What is the name of Abraham Lincoln's dog?","What famous Swiss citizen said of nuclear bombs: 'If I had known, I would have become a watchmaker'?","Which island nation is popstar Rihanna from?","What's Natalie Portman's ACTUAl last name?","How many Hemsworth Brothers are there?"]
     let aroundTheWorldArrayQ: [String] = ["Coffee is originally from...","What country has the most daily newspapers?","What is the largest freshwater lake in the world?","Where would you find the Sea of Tranquility?","What is the world's biggest island?"]
@@ -33,15 +33,24 @@ class QuestionsViewController: UIViewController {
         
         for question in animalsArrayQ
         {
-           print(question)
-            // questionLabel.text = animalsArrayQ
+           //print(question)
+            questionLabel.text = question
+            //goes through the array and prints the last question...but need a random question generater... need to figure out how the button will have the text of answers. need o identify the right answer.
         }
+        
         
     }
     
+    @IBAction func firstChoiceButton(_ sender: UIButton) {
+        
+    }
     
-
-
+    @IBAction func secondChoiceButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func thirdChoiceButton(_ sender: UIButton) {
+    }
+    
     /*
     // MARK: - Navigation
 
