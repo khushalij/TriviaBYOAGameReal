15d84020356dfd645f1c0db181cf9f395dfd557a
Khushali Joshi 2021 <kjoshi21@3012-l29555-s.local>
2019-05-14 09:56:41 -0500

    working on arrays and for loop

--- TriviaBYOAGameReal/DirectionsViewController.swift
+++ TriviaBYOAGameReal/DirectionsViewController.swift
@@ -9,10 +9,7 @@
 import UIKit
 
 class DirectionsViewController: UIViewController {
-    let animalsArrayQ = ["What animal has the longest lifespan?","A newborn kangaroo is about the size of a...?","How many times can a hummingbird flap its wings per second?","What animal has the highest blood pressure?","How mnay noses do slugs have?"]
-    let historyArrayQ = ["When was America discovered?","What Europen language was first spoken in the United States?","What is the real name of Uncle Sam?","Which year was CNN established?","In which state did Thomas Edison die?"]
-    let famousPeopleArrayQ = ["What is the name of Abraham Lincoln's dog?","What famous Swiss citizen said of nuclear bombs: 'If I had known, I would have become a watchmaker'?","Which island nation is popstar Rihanna from?","What's Natalie Portman's ACTUAl last name?","How many Hemsworth Brothers are there?"]
-    let aroundTheWorldArrayQ = ["Coffee is originally from...","What country has the most daily newspapers?","What is the largest freshwater laek in the world?","Where would you find the Sea of Tranquility?","What is the world's biggest island?"]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,8 +24,6 @@ class DirectionsViewController: UIViewController {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let destinationVC = segue.destination as! TopicViewController
-        
-        
     }
     
 }
--- TriviaBYOAGameReal/QuestionsViewController.swift
+++ TriviaBYOAGameReal/QuestionsViewController.swift
@@ -9,14 +9,33 @@
 import UIKit
 
 class QuestionsViewController: UIViewController {
-
+//arrays for questions
+    let animalsArrayQ: [String] = ["What animal has the longest lifespan?","A newborn kangaroo is about the size of a...?","How many times can a hummingbird flap its wings per second?","What animal has the highest blood pressure?","How mnay noses do slugs have?"]
+    let historyArrayQ: [String] = ["When was America discovered?","What Europen language was first spoken in the United States?","What is the real name of Uncle Sam?","Which year was CNN established?","In which state did Thomas Edison die?"]
+    let famousPeopleArrayQ: [String] = ["What is the name of Abraham Lincoln's dog?","What famous Swiss citizen said of nuclear bombs: 'If I had known, I would have become a watchmaker'?","Which island nation is popstar Rihanna from?","What's Natalie Portman's ACTUAl last name?","How many Hemsworth Brothers are there?"]
+    let aroundTheWorldArrayQ: [String] = ["Coffee is originally from...","What country has the most daily newspapers?","What is the largest freshwater lake in the world?","Where would you find the Sea of Tranquility?","What is the world's biggest island?"]
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    var animalQuestion: String = ""
+    var historyQuestion: Array = [0]
+    var famousPeopleQuestion: Array = [0]
+    var aroundTheWorldQuestion: Array = [0]
+    
     @IBOutlet weak var topicLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        for question in animalsArrayQ
+        {
+           print(question)
+            // questionLabel.text = animalsArrayQ
+        }
+        
     }
+    
+    
 
 
     /*
